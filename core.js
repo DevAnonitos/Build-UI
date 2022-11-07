76b605ba37827034005c595dc20d3af0f670bbd7
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2022-11-07 10:40:26 +0700

    update-core

--- core.js
+++ core.js
@@ -6,3 +6,27 @@ export default function html([first, ...strings], ...values) {
     .filter(x => x && x !== true || x === 0)
     .join('')
 }
+
+export function createStore(reducer) {
+    let state = reducer();
+
+    const roots = new Map();
+
+    function render() {
+        for(const [root, component] of roots){
+            const output = component();
+            root.innerHTMl = output
+        }
+    }
+
+    return {
+        attach: function (component, root) {
+            roots.set(root, component)
+            render();
+        },
+        connect(selector = state => state){
+            return component => (props, ...args) =>
+                component(Object.assign(), props, selector(state), ...args)
+        }
+    }
+}
